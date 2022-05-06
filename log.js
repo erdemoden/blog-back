46721058c22a2483f6192b243df07a5f6ea9fe73
erdemoden <erdemoden5@gmail.com>
2022-05-07 01:37:36 +0300

    dsds

--- log.js
+++ log.js
@@ -13,7 +13,7 @@ router.get("/",async(req,res)=>{
                 res.json({"route":"/login"});
             }
             else if(response){
-                res.json({"route":"/homepage"});
+                res.json({"route":"/homepage","username":response.username});
             }
         });
     }
@@ -23,9 +23,9 @@ router.get("/",async(req,res)=>{
                 res.json({"route":"/login"});
             }
             else if(user){
-                let token = jwt.sign({id:user.id},process.env.token_secret,{expiresIn:(10*60)});
+                let token = jwt.sign({id:user.id,username:user.username},process.env.token_secret,{expiresIn:(10*60)});
                 res.cookie('acs',token,{httpOnly:true,maxAge:(10*60)*1000});
-                res.json({"route":"/homepage"});
+                res.json({"route":"/homepage","username":user.username});
             }
         });
         // create jwt using refresh token
@@ -44,16 +44,17 @@ router.post("/signup",async(req,res)=>{
         const post = await user.save();
         const maxage = 10*60;
         const refreshage = 24*60*7;
-        let token = jwt.sign({id:post.id},process.env.token_secret,{expiresIn:maxage});
-        let refresh = jwt.sign({id:post.id},process.env.refresh_secret,{expiresIn:refreshage});
+        let token = jwt.sign({id:user.id,username:user.username},process.env.token_secret,{expiresIn:maxage});
+        let refresh = jwt.sign({id:user.id,username:user.username},process.env.refresh_secret,{expiresIn:refreshage});
         res.cookie('acs',token,{httpOnly:true,maxAge:maxage*1000});
         res.cookie('rfs',refresh,{httpOnly:true,maxAge:refreshage*1000});
-        res.json({"route":"/homepage"});
+        res.json({"route":"/homepage","username":user.username});
     }
     catch(e){
         res.json({'error':'Your Name Should Not Be More Than 15 Characters'});
     }
 });
+
 
 
 
